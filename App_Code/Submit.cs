cdfb44a241ba4ea195e4ad11f2620a914c489e72
HaiLuuVan <luuvanhaiute@gmail.com>
2023-07-26 10:09:32 +0700

    them thu muc

---
+++ App_Code/Submit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        // Chuỗi kết nối đến cơ sở dữ liệu SQL Server
+        string connectionString = "Server=DESKTOP-DELLCUA\\SQLEXPRESS;Database=SCADA_6;User Id=sa;Password=scadanhom7;";
+
+        // Lấy dữ liệu từ form
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Message: ");
+        string message = Console.ReadLine();
+
+        // Kết nối đến cơ sở dữ liệu
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            // Tạo câu lệnh SQL để chèn dữ liệu vào bảng
+            string sql = "INSERT INTO Contact (Name, Email, Message) VALUES (@Name, @Email, @Message)";
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                // Thêm các tham số
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Message", message);
+
+                // Thực hiện câu lệnh SQL
+                command.ExecuteNonQuery();
+            }
+
+            // Đóng kết nối
+            connection.Close();
+        }
+
+        Console.WriteLine("Dữ liệu đã được lưu vào cơ sở dữ liệu.");
+        Console.ReadLine();
+    }
+}
---
+++ Submit.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace MyWebApp
+{
+    public partial class Submit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                string name = Request.Form["Name"];
+                string email = Request.Form["Email"];
+                string message = Request.Form["Message"];
+            
+                SaveDataToDatabase(name, email, message);
+                
+
+                Response.Redirect("HomePage.html");
+            }
+        }
+
+        private void SaveDataToDatabase(string name, string email, string message)
+        {
+            string connectionString = "Data Source=DESKTOP-DELLCUA\\SQLEXPRESS;Initial Catalog=SCADA_6;User ID=sa;Password=scadanhom7";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string sql = "INSERT INTO Contact (Name, Email, Message, Time) VALUES (@Name, @Email, @Message, @Time)";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Email", email);
+                        command.Parameters.AddWithValue("@Message", message);
+                        command.Parameters.AddWithValue("@Time", DateTime.Now);
+
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Xử lý exception nếu có
+                    // Ví dụ: ghi log, hiển thị thông báo lỗi, v.v.
+                    Response.Write("Lỗi kết nối đến cơ sở dữ liệu: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
